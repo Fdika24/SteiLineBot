777a1808c2e4574e5fc5ebf5f47c2aa0f8058143
Farhandika <fdika24@outlook.com>
2020-12-05 15:00:50 +0700

    push 1

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def handle_message(event):
     else:
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(sticker_message),
+            sticker_message,
         )
 
 
