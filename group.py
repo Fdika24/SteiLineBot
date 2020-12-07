e0ea07c0a0a13a45953ef1fd3c44146457eccdc4
Farhandika <fdika24@outlook.com>
2020-12-07 11:16:12 +0700

    add notify

--- group.py
+++ group.py
@@ -6,9 +6,16 @@ from tokens import line_bot_api
 def group_cmd(event):
     user_msg = event.message.text.lower()  # user's msg
     user_token = event.reply_token  # user's token session
-    profile = line_bot_api.get_profile(event.source.user_id)
+    group = event.source.group_id
 
     if user_msg == 'bye eina':
         line_bot_api.reply_message(
             user_token, TextSendMessage(text='Thank you'))
         line_bot_api.leave_group(event.source.group_id)
+
+    if user_msg == 'notify_me':  # this will push something to the user , to be continue
+        line_bot_api.push_message(
+            group, [
+                TextSendMessage(text='PUSH!'),
+            ]
+        )
