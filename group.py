24f375d609a8298cd140d60c1f1c8914024ca26b
Farhandika <fdika24@outlook.com>
2020-12-07 11:27:58 +0700

    add push

--- group.py
+++ group.py
@@ -6,16 +6,24 @@ from tokens import line_bot_api
 def group_cmd(event):
     user_msg = event.message.text.lower()  # user's msg
     user_token = event.reply_token  # user's token session
-    group = event.source.group_id
+    group = event.source.group_id # group id
 
     if user_msg == 'bye eina':
         line_bot_api.reply_message(
             user_token, TextSendMessage(text='Thank you'))
         line_bot_api.leave_group(event.source.group_id)
 
-    if user_msg == 'notify_me':  # this will push something to the user , to be continue
+    if user_msg == 'getdata':
+        line_bot_api.reply_message(
+            user_token,
+            TextSendMessage(text=f'Group id : {group}')
+        )
+
+    if user_msg.startwith('push '):
+        data = user_msg.split(' ')[1:]  # this will push something to the user , to be continue
+        group = data[0]
         line_bot_api.push_message(
             group, [
-                TextSendMessage(text='PUSH!'),
+                TextSendMessage(text=' '.join(data[1:])),
             ]
         )
